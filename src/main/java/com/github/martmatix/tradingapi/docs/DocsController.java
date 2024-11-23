f5ef765362555fadeec281a87132c4e4befaf592
MartmatiX <martin.malir@gmail.com>
2024-11-23 22:46:02 +0100

    Removal of hosts from documentation

--- src/main/java/com/github/martmatix/tradingapi/docs/DocsController.java
+++ src/main/java/com/github/martmatix/tradingapi/docs/DocsController.java
@@ -1,16 +1,26 @@
 package com.github.martmatix.tradingapi.docs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 @Controller
+@OpenAPIDefinition(
+        info = @Info(title = "Inventory API"),
+        servers = {@Server(description = "Trading API Docs")}
+)
 public class DocsController {
 
     private WebClient.Builder builder;
@@ -27,6 +37,11 @@ public class DocsController {
                 .bodyToMono(String.class);
         model.addAttribute("apiDocs", Objects.requireNonNull(stringMono.block()));
         return "docs";
+    }
+
+    @Bean
+    public OpenApiCustomizer hideServers() {
+        return openApi -> openApi.setServers(new ArrayList<>());
     }
 
     @Autowired
